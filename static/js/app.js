84de9dc96ced3b330ee717bcdbd71bb23d1cf7cd
peytonmford <peyton.m.ford@ttu.edu>
2020-12-05 19:52:19 -0600

    added index.html

--- static/js/app.js
+++ static/js/app.js
@@ -23,3 +23,16 @@ function buildTable(data) {
     );
   });
 }
+
+function handleClick() {
+  let date = d3.select("#datetime").property("value");
+  let filteredData = tableData;
+  if (date) {
+    filteredData = filteredData.filter(row => row.datetime == date);
+  }
+
+  buildTable(filteredData);
+}
+
+d3.selectAll("#filter-btn").on("click", handleClick);
+buildTable(tableData);
